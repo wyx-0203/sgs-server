949ad18a157d519637a4a09fdb355fc226a859ba
Yuxi Wang <3081879742@qq.com>
2023-04-01 23:46:34 +0800

    init

--- global/constants.go
+++ global/constants.go
@@ -13,8 +13,8 @@ const (
 	// UserServiceName         = "demouser"
 	// UserServiceAddr = "localhost:10001"
 	// MYSQL_DSN = "sgs:123456@tcp(localhost:3306)/sgs?charset=utf8&loc=Local"
-	MYSQL_DSN = "sgs:123456@tcp(host.docker.internal:3306)/sgs?charset=utf8&parseTime=True&loc=Local"
-	// MYSQL_DSN = "sgs:123456@tcp(localhost:3306)/sgs?charset=utf8&parseTime=True&loc=Local"
+	// MYSQL_DSN = "sgs:123456@tcp(host.docker.internal:3306)/sgs?charset=utf8&parseTime=True&loc=Local"
+	MYSQL_DSN = "sgs:123456@tcp(123.56.19.80:3306)/sgs?charset=utf8&parseTime=True&loc=Local"
 	// EtcdAddress             = "127.0.0.1:2379"
 	// CPURateLimit    float64 = 80.0
 	// DefaultLimit            = 10
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/wyx-0203/sgs-server
 
-go 1.19
+go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.2
